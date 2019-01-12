a7172bb63f4bb83f8d9430bab3dba193a2cadefe
ne2030 <erguono@gmail.com>
2019-01-13 02:21:20 +0900

    codewars - church / algebraic data types 문제

---
+++ 2019/jan/12_codeWars.js
@@ -0,0 +1,54 @@
+function zero() { }
+function succ(nat) {
+    return () => nat
+}
+function isZero(nat) {
+    return !nat()
+}
+
+function natToInt(nat) {
+    return (function sol(n) {
+        n() ? 1 + sol(n()) : 0;
+    })(nat);
+}
+
+function intToNat(int) {
+    return !int ? zero : succ(intToNat(int - 1))
+}
+
+function add(nat1, nat2) {
+    return nat1() ? add(nat1(), succ(nat2)) : nat2;
+}
+
+function add(nat1, nat2) {
+    return nat1() ? () => add(nat1(), nat2) : nat2;
+}
+
+
+function mul(nat1, nat2) {
+    return !nat1() ? zero :
+        isZero(nat1()) ? nat2 :
+            add(mul(nat1(), nat2), nat2);
+}
+
+function compareTo(nat1, nat2) {
+    return !nat1() && !nat2() ? 0 :
+        !nat1() && nat2() ? -1 :
+            nat1() && !nat2() ? 1 :
+                compareTo(nat1(), nat2())
+}
+
+function compareTo(nat1, nat2) {
+    
+}
+
+function toString(nat) {
+    return nat() ? `succ(${toString(nat())})` : 'zero';
+}
+
+const n5000 = intToNat(5000)
+console.log(
+
+);
+
+// add(intToNat(5000), intToNat(5000))
